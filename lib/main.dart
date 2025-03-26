acfdad2759278d734fa6104af634187cad64a940
Ruan Pactrick <ruanpetrick18@gmail.com>
2025-03-26 16:19:09 -0300

    olamundo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/perfilCoordenador.dart';
-import 'package:flutter_application_1/screens/perfilProfessor.dart';
 
 void main() {
   runApp(const MyApp());
